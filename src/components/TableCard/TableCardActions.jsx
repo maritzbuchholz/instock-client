455b014fb4fcbf4b7e81957f0395291db62bc641
Kathy Yeung <kathyyeung7@gmail.com>
2026-02-07 17:44:50 -0500

    Created the inventory list component to display a list of all inventories. updated the icons to use the iconography.

--- src/components/TableCard/TableCardActions.jsx
+++ src/components/TableCard/TableCardActions.jsx
@@ -1,13 +1,11 @@
 import { Link } from "react-router-dom";
-import Icon from "../Iconography/Iconography";
-import editIcon from "../../assets/Icons/edit24px.svg";
-import deleteIcon from "../../assets/Icons/deleteoutline24px.svg";
+import Iconography from "../Iconography/Iconography";
 
 const TableCardActions = ({ editTo, onDelete, className = "" }) => {
     return (
         <div className={`card__actions ${className}`}>
             <Link to={editTo} className="card__icon-action" aria-label="Edit">
-                <Icon name="edit" />
+                <Iconography name="edit" />
             </Link>
 
             <button
@@ -16,7 +14,7 @@ const TableCardActions = ({ editTo, onDelete, className = "" }) => {
                 aria-label="Delete"
                 onClick={onDelete}
             >
-                <Icon name="delete" />
+                <Iconography name="delete" />
             </button>
         </div>
     );
--- src/components/TablesInventory/TablesInventory.jsx
+++ src/components/TablesInventory/TablesInventory.jsx
@@ -1,1 +1,52 @@
+import { Link } from "react-router-dom";
+import Iconography from "../Iconography/Iconography";
+import TableCard from "../TableCard/TableCard"; 
+import TableCardField from "../TableCard/TableCardField";
+import TableCardActions from "../TableCard/TableCardActions";
+import Tags from "../Tags/Tags";
 import "./TablesInventory.scss";
+
+const TablesInventory = ({ inventory }) => {
+    if (!inventory || inventory.length === 0) {
+        return <p>No inventory available.</p>;
+    }
+
+    return (
+        <div className="inventory-table">
+            {inventory.map((item) => (
+                <TableCard key={item.id} className="inventory-table__Card">
+                    <TableCardField label="INVENTORY ITEM" className="inventory-table__field">
+                        <Link to={`/inventory/${item.id}`} className="inventory-link">
+                            {item.item_name}
+                            <Iconography name="chevronRight" className="inventory-link__icon" />
+                        </Link>
+                    </TableCardField>
+
+                    <TableCardField label="CATEGORY" className="inventory-table__field">
+                        {item.category}
+                    </TableCardField>
+
+                    <TableCardField label="STATUS" className="inventory-table__field">
+                        <Tags status={item.status} />
+                    </TableCardField>
+
+                    <TableCardField label="QTY" className="inventory-table__field">
+                        {item.quantity}
+                    </TableCardField>
+
+                    <TableCardField label="WAREHOUSE" className="inventory-table__field">
+                        {item.warehouse_name}
+                    </TableCardField>
+
+                    <TableCardActions
+                        editTo={`/inventory/${item.id}/edit`}
+                        onDelete={() => console.log("Delete inventory", item.id)}
+                        className="inventory-table__actions"
+                    />
+                </TableCard>
+            ))}
+        </div>
+    );
+};
+
+export default TablesInventory;
