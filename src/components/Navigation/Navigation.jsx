c6e803fd6ac7a27674df5d41a890227d3fbe57eb
Renee Vettivelu <renee.vettivelu08@gmail.com>
2026-02-07 10:31:37 -0500

    added nav component html and renamed logos in assets folder

--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -1,9 +1,39 @@
 import { NavLink } from "react-router-dom";
+import InStockLogo from "../../assets/Logo/InStockLogo.svg";
 import "./Navigation.scss";
+import Typography from "../Typography/Typography";
 
 const Navigation = () => {
     return (
-       <></>
+       <header className = "nav">
+        <div className = "nav__logo">
+            <img src={InStockLogo} alt="InStock Logo" />
+        </div>
+
+        <nav className = "nav__tabs">
+            <NavLink
+                to="/warehouses"
+                className={({  isActive  }) =>
+                    isActive ? "nav_tab nav__tab--active"
+                : "nav_tab"
+                }
+            >
+            Warehouses
+            </NavLink>
+
+            <NavLink
+                to="/inventory"
+                className={({  isActive  }) =>
+                    isActive ? "nav_tab nav__tab--active"
+                : "nav_tab"
+                }
+            >
+            Inventory
+            </NavLink>
+
+        </nav>
+
+       </header>
     )
 
 };
