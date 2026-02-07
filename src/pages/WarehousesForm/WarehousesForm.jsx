19ac983c5c4200c4a808a4b47e3a92c1a03fe4e0
maritzbuchholz <maritzbuchholz@gmail.com>
2026-02-07 12:09:44 -0500

    continue to build out warehouse form

--- src/pages/WarehousesForm/WarehousesForm.jsx
+++ src/pages/WarehousesForm/WarehousesForm.jsx
@@ -9,20 +9,24 @@ const WarehousesForm = () => {
                 <Typography variant="h1">Add New Warehouse</Typography>
             </div>
 
-            <div className="warehouses-form__warehouse-details">
-                <Typography variant="h1">Warehouse Details</Typography>
-                <FormFields inputName="Warehouse Name"/>
-                <FormFields inputName="Street Address"/>
-                <FormFields inputName="City"/>
-                <FormFields inputName="Country"/>
-            </div>
-            <div className="warehouses-contact-details">
-                <Typography variant="h1">Contact Details</Typography>
-                <FormFields inputName="Contact Name"/>
-                <FormFields inputName="Position"/>
-                <FormFields inputName="Phone Number"/>
-                <FormFields inputName="Email"/>
-            </div>
+            <form className="warehouses-form__wrapper">
+                <div className="warehouses-form__warehouse-details">
+                    <Typography variant="h2">Warehouse Details</Typography>
+                    <FormFields inputName="Warehouse Name"/>
+                    <FormFields inputName="Street Address"/>
+                    <FormFields inputName="City"/>
+                    <FormFields inputName="Country"/>
+                </div>
+
+                <div className="warehouses-form__contact-details">
+                    <Typography variant="h2">Contact Details</Typography>
+                    <FormFields inputName="Contact Name"/>
+                    <FormFields inputName="Position"/>
+                    <FormFields inputName="Phone Number"/>
+                    <FormFields inputName="Email"/>
+                </div>
+            </form>
+            
         </section>
     );
 };
