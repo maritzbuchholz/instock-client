0a52e9f22b0198bc57e52871a969433c5ce6487e
Kavin Paul <kavinp622@gmail.com>
2026-02-08 12:34:02 -0700

    add table header component that contains the header, search input and button

--- src/pages/WarehousesPage/WarehousesPage.jsx
+++ src/pages/WarehousesPage/WarehousesPage.jsx
@@ -1,13 +1,12 @@
 import "./WarehousesPage.scss";
 import Typography from "../../components/Typography/Typography.jsx";
 import TableWarehouses from "../../components/TablesWarehouses/TablesWarehouses.jsx";
+import TableHeader from "../../components/TablesHeader/TablesHeader.jsx";
 
 const WarehousesPage = ({ warehouses }) => {
     return (
         <section className="warehouses">
-            <div className="warehouses__header">
-                <Typography variant="h1">Warehouses</Typography>
-            </div>
+            <TableHeader headerText="Warehouses" buttonText="+ Add New Warehouse" />
 
             <div className="warehouses__content">
                 <TableWarehouses warehouses={warehouses} />
