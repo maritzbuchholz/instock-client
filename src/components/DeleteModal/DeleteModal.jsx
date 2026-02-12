188a8fadc0c68b18d6d9bbfae5e9e30d20428c95
Kavin Paul <kavinp622@gmail.com>
2026-02-11 19:51:43 -0700

    initial commit for DeleteModal

---
+++ src/components/DeleteModal/DeleteModal.jsx
@@ -0,0 +1,12 @@
+import Typography from "../../components/Typography/Typography.jsx";
+import "./DeleteModal.scss";
+
+const DeleteModal = (deleteItem, variant = "warehouse", onCancel, onConfirm) => { //variants = warehouse, inventory
+    return (
+        <div className="delete-modal__wrapper">
+
+        </div>
+    );
+};
+
+export default DeleteModal;
