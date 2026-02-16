e9b2856175aeecf47cc66f0468bfcf4b182ac456
maritzbuchholz <maritzbuchholz@gmail.com>
2026-02-16 16:31:02 -0500

    sent form data to server and update database successfully; had to update naming convention in front end so will come back to that

--- src/pages/WarehousesForm/WarehousesForm.jsx
+++ src/pages/WarehousesForm/WarehousesForm.jsx
@@ -1,24 +1,28 @@
 import "./WarehousesForm.scss";
+import axios from "axios";
 import Typography from "../../components/Typography/Typography.jsx";
 import FormFields from "../../components/FormFields/FormFields.jsx";
 import Button from "../../components/Button/Button.jsx";
 import { emptyFieldError, validateEmail, validatePhone, removeErrors, formatPhoneInput } from "../../utils/formValidation.js";
 import { useState, useEffect } from "react";
 
+const baseUrl = import.meta.env.VITE_API_BASE_URL;
 
-// This useEffect manages all potential types
-// Include field name below if you want the field to be required or validated,
-// Set intial error state to empty string
-// FormFields set name = id
+
 const WarehousesForm = () => {
+    // This useEffect manages all potential types
+    // Include field name below if you want the field to be required or validated,
+    // Set intial error state to empty string
+    // FormFields set name = id
     const [errors, setError] = useState({ 
-        "warehouse-name": "",
+        "warehouse_name": "",
         "address": "",
         "city": "",
         "country": "",
-        "contact-name": "",
-        "phone": "",
-        "email": "",
+        "contact_name": "",
+        "contact_position": "",
+        "contact_phone": "",
+        "contact_email": "",
     });
 
     const handleSubmit = async(e)=> {
@@ -37,13 +41,16 @@ const WarehousesForm = () => {
             setError(newErrors);
             return; 
         } else if (!errorExists) {
-            console.log("submit form");
+            const formData = new FormData(e.currentTarget);
+            const serverData = Object.fromEntries(formData.entries());
+            console.log(serverData);
+            axios.post(`${baseUrl}/warehouses`, serverData);
         };
     };
 
     const handleChange = (e) => { 
         removeErrors(e, errors, setError); // Any errors flag are removed once user interacts
-        if (e.currentTarget.name === "phone") {
+        if (e.currentTarget.name === "contact_phone") {
             e.currentTarget.value = formatPhoneInput(e.currentTarget.value); // Restricts phone number format live
         };
     };
@@ -66,7 +73,7 @@ const WarehousesForm = () => {
 
                 <div className="warehouses-form__warehouse-details">
                     <Typography className="warehouses-form__typography-text--form" variant="h2">Warehouse Details</Typography>
-                    <FormFields onChange={handleChange} errorState={errors["warehouse-name"]} htmlFor="warehouse-name" inputName="Warehouse Name" />
+                    <FormFields onChange={handleChange} errorState={errors["warehouse_name"]} htmlFor="warehouse_name" inputName="Warehouse Name" />
                     <FormFields onChange={handleChange} errorState={errors["address"]} htmlFor="address" inputName="Street Address"/>
                     <FormFields onChange={handleChange} errorState={errors["city"]} htmlFor="city" inputName="City"/>
                     <FormFields onChange={handleChange} errorState={errors["country"]} htmlFor="country" inputName="Country"/>
@@ -74,10 +81,10 @@ const WarehousesForm = () => {
 
                 <div className="warehouses-form__contact-details">
                     <Typography className="warehouses-form__typography-text--form" variant="h2">Contact Details</Typography>
-                    <FormFields onChange={handleChange} errorState={errors["contact-name"]} htmlFor="contact-name" inputName="Contact Name"/>
-                    <FormFields htmlFor="position" inputName="Position"/>
-                    <FormFields onChange={handleChange} errorState={errors["phone"]} htmlFor="phone" inputName="Phone Number"/>
-                    <FormFields onChange={handleChange} errorState={errors["email"]} htmlFor="email" inputName="Email"/>
+                    <FormFields onChange={handleChange} errorState={errors["contact_name"]} htmlFor="contact_name" inputName="Contact Name"/>
+                    <FormFields onChange={handleChange} errorState={errors["contact_position"]} htmlFor="contact_position" inputName="Position"/>
+                    <FormFields onChange={handleChange} errorState={errors["contact_phone"]} htmlFor="contact_phone" inputName="Phone Number"/>
+                    <FormFields onChange={handleChange} errorState={errors["contact_email"]} htmlFor="contact_email" inputName="Email"/>
                 </div>
 
             </section>
